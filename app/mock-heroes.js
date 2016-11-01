0a89dd27e36ddc9d1bcdfdab28bc6d43755e7353
hsky <wen.zhou@bioon.com>
2016-11-01 17:57:07 +0800

    angular2 快速上手项目

---
+++ app/mock-heroes.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.HEROES = [
+    { id: 11, name: "jjjjj" },
+    { id: 12, name: "AAAAA" },
+    { id: 13, name: "BBBBB" },
+    { id: 14, name: "CCCCC" },
+    { id: 15, name: "DDDDD" },
+    { id: 16, name: "EEEEE" },
+    { id: 17, name: "FFFFF" },
+    { id: 18, name: "GGGGG" },
+    { id: 19, name: "HHHHH" },
+    { id: 20, name: "IIIII" }
+];
+//# sourceMappingURL=mock-heroes.js.map
---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES : Hero[] = [
+	{id : 11, name : "jjjjj"},
+	{id : 12, name : "AAAAA"},
+	{id : 13, name : "BBBBB"},
+	{id : 14, name : "CCCCC"},
+	{id : 15, name : "DDDDD"},
+	{id : 16, name : "EEEEE"},
+	{id : 17, name : "FFFFF"},
+	{id : 18, name : "GGGGG"},
+	{id : 19, name : "HHHHH"},
+	{id : 20, name : "IIIII"}
+];
